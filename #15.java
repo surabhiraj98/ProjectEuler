34068fa4e68dd70eb9f37cc715773b220b3bf684
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-09-27 22:30:19 +0530

    Create #15.java

---
+++ #15.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner in = new Scanner(System.in);
+        int numberOfTestCases = in.nextInt();
+        
+        for(int i=0;i<numberOfTestCases;i++)
+            {
+            int n = in.nextInt();
+            int m = in.nextInt();  
+            BigInteger nPlusMfact = getFactorial(n+m);
+            BigInteger nFact = getFactorial(n);
+            BigInteger mFact = getFactorial(m);
+            BigInteger nFactIntoMFact = nFact.multiply(mFact);
+            BigInteger res = nPlusMfact.divide(nFactIntoMFact);
+            BigInteger modVal = BigInteger.valueOf(1000000007);
+            System.out.println(res.mod(modVal));
+        }
+    }
+    public static BigInteger getFactorial(int n)
+        {
+        if(n==1) return BigInteger.valueOf(1);
+        BigInteger fact = BigInteger.valueOf(1);
+        for(int i=2;i<=n;i++)
+            {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        return fact;
+    }
+}
+
