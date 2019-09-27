148c4d418ec6189d3f149d67dbbe3710a1223b42
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-09-27 22:08:21 +0530

    Create #4.java

---
+++ #4.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfTestCases = in.nextInt();
+        for(int i=0;i<numberOfTestCases;i++)
+            {
+            long n = in.nextLong();
+            long maxProduct = 1L;
+            for(long j=999L;j>100L;j--)
+                {
+                for(long k=990L;k%11==0 && k>100;k-=11)
+                    {
+                    long product = k*j;
+                    String productString = Long.toString(product);
+                    String reversedProductString = new StringBuilder(productString).reverse().toString();
+                    if(productString.length()==6 && productString.equals(reversedProductString) && product<n)
+                        {
+                        //System.out.println(product + " " + j + ":" + k);
+                        if(product>maxProduct)
+                            {
+                            maxProduct = product;
+                        }
+                    }
+                }
+            }
+            System.out.println(maxProduct);
+        }
+    }
+}
